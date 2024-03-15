1b7269aa131fd40bfa09abaeb12c54653e5ac9d6
Mahendra Shinde <MahendraShinde@synegertics-india.com>
2024-03-15 15:00:12 +0530

    Updated with PUT and POST endpoints

--- demo-4/src/main/java/com/mahendra/demo4/AccountResource.java
+++ demo-4/src/main/java/com/mahendra/demo4/AccountResource.java
@@ -1,8 +1,11 @@
 package com.mahendra.demo4;
 
 import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
+
+import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
 
 
 @Path("/account")
@@ -14,13 +17,33 @@ public class AccountResource {
 	@GET
 	@Path("/{id}")
 	public Response findById(@PathParam("id") Integer id) {
-		
 		try {
 		Account acc = dao.findById(id); // Method might throw an exception
 		return Response.ok(acc).build();
 		}catch(RuntimeException ex) {
 			return Response.status(Status.NOT_FOUND).entity(ex.getMessage()).build();
 		}
-		
 	}
-}
+	
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON) /// Alternative to @Consumes("application/json")
+	public Response create(Account acc) {
+		try {
+		dao.save(acc);
+		return Response.ok("Created new account !").build();
+		}catch(RuntimeException ex) {
+			return Response.status(Status.BAD_REQUEST).entity(ex.getMessage()).build();
+		}
+	}
+	
+	@PUT
+	@Path("/{id}")
+	public Response update(@PathParam("id") Integer id,@RequestBody Account acc) {
+		try {
+			dao.update(id, acc);
+			return Response.ok("Account Updated !").build();
+			}catch(RuntimeException ex) {
+				return Response.status(Status.BAD_REQUEST).entity(ex.getMessage()).build();
+			}
+	}
+}	
